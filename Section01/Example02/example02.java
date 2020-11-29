cdf9715dcb94aff1d9ecf1f75252f634af5977d8
liulawsi <liulawsi@gmail.com>
2020-11-29 15:30:48 +0800

    Upload First Time

---
+++ Section01/Example02/example02.java
@@ -0,0 +1,18 @@
+
+public class example02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t床前明月光  疑似地上霜");
+		System.out.println("\t舉頭望明月  低頭思便當");
+		System.out.println("\t\t\t李黑 靜夜思");
+		System.out.println("");
+		System.out.println("  低  舉  疑  床");
+		System.out.println("  頭  頭  似  前  李");
+		System.out.println("  思  望  地  明  黑");
+		System.out.println("  便  明  上  月");
+		System.out.println("  當  月  霜  光  靜");
+		System.out.println("                 夜");
+		System.out.println("                 思");
+	}
+}
---
+++ Section01/Example03/example03.java
@@ -0,0 +1,16 @@
+package example03;
+
+import java.util.Scanner;
+
+public class example03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name;
+		System.out.print("請問大名是？");
+		name = sc.nextLine();
+		System.out.println("Hello, " + name + " !");
+	}
+
+}
