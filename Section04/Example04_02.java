778c89c49e9cddfb64f709552313691443a79836
liulawsi <liulawsi@gmail.com>
2020-11-29 16:17:41 +0800

    reference codes

---
+++ Section04/Example04_02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Example04_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int number;
+		
+		System.out.print("請輸入數字：");
+		number = sc.nextInt();
+		
+		if (number % 2 == 0) System.out.println(+number+"是2的倍數");
+		else System.out.println(+number+"不是2的倍數");
+		if (number % 3 == 0) System.out.println(+number+"是3的倍數");
+		else System.out.println(+number+"不是3的倍數");
+	}
+
+}
